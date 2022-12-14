7e82233f785d4c426b6e79efc3bc31887148ea84
user <sheep3724@hanyang.ac.kr>
2022-12-14 11:38:59 +0900

    Chatbot

--- chatbot/views.py
+++ chatbot/views.py
@@ -15,7 +15,7 @@ def keyboard(request):
 def message(request):
     answer = (request.body.decode('utf-8'))
     return_json_str = json.loads(answer)
-    return_str = return_json_str['userRequest']['utterance']
+    return_str = return_json_str['utterance']
 
     if return_str == 'test':
         return JsonResponse({
