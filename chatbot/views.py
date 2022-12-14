7792976e41dd47e94a11a93220d1dc905d9b9fe0
user <sheep3724@hanyang.ac.kr>
2022-12-14 11:05:26 +0900

    Chatbot

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Create your views here.
+def keyboard(request):
+    return JsonResponse({
+        'type': 'text'
+    })
