e5860c1bcbdb85cf7ca5f8a781efd782caafd129
user <sheep3724@hanyang.ac.kr>
2022-12-31 00:21:11 +0900

    Chatbot

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
 from chatbot.models import User_Model, Store_Model
-
-# Register your models here.
 admin.site.register(User_Model)
 admin.site.register(Store_Model)
--- chatbot/views.py
+++ chatbot/views.py
@@ -3,7 +3,7 @@ import os
 from django.shortcuts import render
 
 # Create your views here.
-
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
