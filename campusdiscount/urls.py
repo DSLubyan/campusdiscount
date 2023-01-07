0d58706853296e6a7e9681923b13c1cd16c42750
user <sheep3724@hanyang.ac.kr>
2023-01-07 14:56:24 +0900

    Chatbot

--- campusdiscount/urls.py
+++ campusdiscount/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('keyboard/',chatbot.views.keyboard),
+    path('keyboard',chatbot.views.keyboard),
     path('message', chatbot.views.message)
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
